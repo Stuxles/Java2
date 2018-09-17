7cdda56f24b51671745cb372423d9aec5657f0cb
Stuxles <stuxles@live.nl>
2018-09-17 23:14:53 +0200

    scribble

---
+++ scribble/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args){
+        DrawDemo drawDemo = new DrawDemo();
+        drawDemo.drawSquare();
+        drawDemo.colorScribble();
+        drawDemo.drawWheel();
+    }
+}
