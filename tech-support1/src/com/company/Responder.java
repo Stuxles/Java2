e5b54127c7a565df7ca82dab5046620deb23f166
Stuxles <stuxles@live.nl>
2018-09-17 22:41:29 +0200

    test

--- tech-support1/src/com/company/Responder.java
+++ tech-support1/src/com/company/Responder.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -13,7 +14,8 @@ import java.util.Random;
 public class Responder
 {
     private Random randomGenerator;
-    private ArrayList<String> responses;
+    private ArrayList<String> defaultResponses;
+    private HashMap<String, String> responseMap;
 
     /**
      * Construct a Responder - nothing to do
@@ -21,32 +23,99 @@ public class Responder
     public Responder()
     {
         randomGenerator = new Random();
-        responses = new ArrayList<>();
-        fillResponses();
+        defaultResponses = new ArrayList<>();
+        filldefaultResponses();
+        responseMap = new HashMap<>();
+        fillResponseMap();
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
+    public String generateResponse(String word)
     {
-        int index = randomGenerator.nextInt(responses.size());
-        return  responses.get(index);
+        String response = responseMap.get(word);
+        if (response != null) {
+            return response;
+        }
+        else {
+            return pickDefaultResponses();
+        }
     }
-    private void fillResponses()
+
+    private void fillResponseMap()
     {
-        responses.add("That sounds odd. Could you describe this in more detail?");
-        responses.add("No other customer has ever complained about this \n" +
+        responseMap.put("crash",
+                "Well, it never crashes on our system. It must have something\n" +
+                        "to do with your system. Tell me more about your configuration.");
+        responseMap.put("crashes",
+                "Well, it never crashes on our system. It must have something\n" +
+                        "to do with your system. Tell me more about your configuration.");
+        responseMap.put("slow",
+                "I think this has to do with your hardware. Upgrading your processor\n" +
+                        "should solve all performance problems. Have you got a problem with\n" +
+                        "our software?");
+        responseMap.put("performance",
+                "Performance was quite adequate in all our tests. Are you running\n" +
+                        "any other processes in the background?");
+        responseMap.put("bug",
+                "Well, you know, all software has some bugs. But our software engineers\n" +
+                        "are working very hard to fix them. Can you describe the problem a bit\n" +
+                        "further?");
+        responseMap.put("buggy",
+                "Well, you know, all software has some bugs. But our software engineers\n" +
+                        "are working very hard to fix them. Can you describe the problem a bit\n" +
+                        "further?");
+        responseMap.put("windows",
+                "This is a known bug to do with the Windows operating system. Please\n" +
+                        "report it to Microsoft. There is nothing we can do about this.");
+        responseMap.put("mac",
+                "This is a known bug to do with the Mac operating system. Please\n" +
+                        "report it to Apple. There is nothing we can do about this.");
+        responseMap.put("expensive",
+                "The cost of our product is quite competitive. Have you looked around\n" +
+                        "and really compared our features?");
+        responseMap.put("installation",
+                "The installation is really quite straight forward. We have tons of\n" +
+                        "wizards that do all the work for you. Have you read the installation\n" +
+                        "instructions?");
+        responseMap.put("memory",
+                "If you read the system requirements carefully, you will see that the\n" +
+                        "specified memory requirements are 1.5 giga byte. You really should\n" +
+                        "upgrade your memory. Anything else you want to know?");
+        responseMap.put("linux",
+                "We take Linux support very seriously. But there are some problems.\n" +
+                        "Most have to do with incompatible glibc versions. Can you be a bit\n" +
+                        "more precise?");
+        responseMap.put("bluej",
+                "Ahhh, BlueJ, yes. We tried to buy out those guys long ago, but\n" +
+                        "they simply won't sell... Stubborn people they are. Nothing we can\n" +
+                        "do about it, I'm afraid.");
+    }
+
+    private void filldefaultResponses()
+    {
+        defaultResponses.add("That sounds odd. Could you describe this in more detail?");
+        defaultResponses.add("No other customer has ever complained about this \n" +
                 "before. What is your system configuration?");
-        responses.add("I need a bit more information on that.");
-        responses.add("Have you checked that you do not have a dll conflict?");
-        responses.add("That is covered in the manual. Have you read the manual?");
-        responses.add("Your description is a bit wishy-washy. Have you got \n" +
+        defaultResponses.add("I need a bit more information on that.");
+        defaultResponses.add("Have you checked that you do not have a dll conflict?");
+        defaultResponses.add("That is covered in the manual. Have you read the manual?");
+        defaultResponses.add("Your description is a bit wishy-washy. Have you got \n" +
                 "an expert there with you who could describe this better?");
-        responses.add("That's not a bug, it's a feature!");
-        responses.add("Could you elaborate on that?");
-        responses.add("Have you tried running the app on your phone?");
-        responses.add("I just checked StackOverflow - they don't know either.");
+        defaultResponses.add("That's not a bug, it's a feature!");
+        defaultResponses.add("Could you elaborate on that?");
+        defaultResponses.add("Have you tried running the app on your phone?");
+        defaultResponses.add("I just checked StackOverflow - they don't know either.");
     }
+
+    private String pickDefaultResponses()
+    {
+        // Pick a random number for the index in the default response list.
+        // The number will be between 0 (inclusive) and the size of the list (exclusive).
+        int index = randomGenerator.nextInt(defaultResponses.size());
+        return defaultResponses.get(index);
+    }
+
 }
