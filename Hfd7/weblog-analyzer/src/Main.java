ec8198cc67dbcf7d867b42f295871e026d748746
Stuxles <stuxles@live.nl>
2018-09-24 21:59:58 +0200

    New

---
+++ Hfd7/weblog-analyzer/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        LogAnalyzer logAnalyzer = new LogAnalyzer();
+        logAnalyzer.analyzeHourlyData();
+        logAnalyzer.printHourlyCounts();
+    }
+}
